3aa8404519bc943cb7a2c6e0fd26661cfeff261a
wurongfeng1995 <wurongfeng@nd.com.cn>
2024-10-11 22:34:16 +0800

    a

--- app/src/main/java/com/aom_ai/uc_app/MainActivity.kt
+++ app/src/main/java/com/aom_ai/uc_app/MainActivity.kt
@@ -5,14 +5,14 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
-import com.aom_ai.main_page.HomePageActivity
+import com.aom_ai.learning.LearningActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Handler(Looper.getMainLooper()).postDelayed({
-            startActivity(Intent(this, HomePageActivity::class.java))
+            startActivity(Intent(this, LearningActivity::class.java))
             finish()
         }, 2000)
     }
